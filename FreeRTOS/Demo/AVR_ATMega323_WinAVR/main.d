74c088d2b991f594aba30be1b49137b69d0d86c5
tprihoda <prihodat@oregonstate.edu>
2020-02-15 15:25:59 -0800

    updated for Atmega32u4

--- FreeRTOS/Demo/AVR_ATMega323_WinAVR/main.d
+++ FreeRTOS/Demo/AVR_ATMega323_WinAVR/main.d
@@ -3,8 +3,10 @@ main.o main.d : main.c ../../Source/include/FreeRTOS.h FreeRTOSConfig.h \
  ../../Source/include/deprecated_definitions.h \
  ../../Source/include/../portable/GCC/ATMega323/portmacro.h \
  ../../Source/include/mpu_wrappers.h ../../Source/include/task.h \
- ../../Source/include/list.h ../../Source/include/croutine.h \
- ../Common/include/PollQ.h ../Common/include/integer.h \
- ../Common/include/serial.h ../Common/include/comtest.h \
- ../Common/include/crflash.h ../Common/include/print.h \
- ../Common/include/partest.h regtest.h tcp/tcp_server.h
+ ../../Source/include/list.h tcp/tcp_server.h \
+ ../../../lib/ioLibrary_Driver/Ethernet/socket.h \
+ ../../../lib/ioLibrary_Driver/Ethernet/wizchip_select.h \
+ ../../../lib/ioLibrary_Driver/Ethernet/wizchip_conf.h \
+ ../../../lib/ioLibrary_Driver/Ethernet/W5500/w5500.h \
+ ../../../lib/ioLibrary_Driver/Ethernet/W5500/../wizchip_select.h \
+ ../../../lib/ioLibrary_Driver/Ethernet/wizchip_conf.h uart.h
--- FreeRTOS/Demo/AVR_ATMega323_WinAVR/tcp/tcp_server.d
+++ FreeRTOS/Demo/AVR_ATMega323_WinAVR/tcp/tcp_server.d
@@ -1,7 +1,13 @@
-tcp_server.o tcp_server.d : tcp/tcp_server.c tcp/tcp_server.h \
+tcp_server.o tcp_server.d : tcp/tcp_server.c ../../Source/include/FreeRTOS.h \
+ FreeRTOSConfig.h ../../Source/include/projdefs.h \
+ ../../Source/include/portable.h \
+ ../../Source/include/deprecated_definitions.h \
+ ../../Source/include/../portable/GCC/ATMega323/portmacro.h \
+ ../../Source/include/mpu_wrappers.h ../../Source/include/task.h \
+ ../../Source/include/list.h tcp/tcp_server.h \
  ../../../lib/ioLibrary_Driver/Ethernet/socket.h \
  ../../../lib/ioLibrary_Driver/Ethernet/wizchip_select.h \
  ../../../lib/ioLibrary_Driver/Ethernet/wizchip_conf.h \
  ../../../lib/ioLibrary_Driver/Ethernet/W5500/w5500.h \
  ../../../lib/ioLibrary_Driver/Ethernet/W5500/../wizchip_select.h \
- ../../../lib/ioLibrary_Driver/Ethernet/wizchip_conf.h uart.h
+ ../../../lib/ioLibrary_Driver/Ethernet/wizchip_conf.h uart.h tcp/jsmn.h
