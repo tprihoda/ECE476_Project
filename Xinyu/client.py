5e42cee2ad5e19576ef11a75e7c7aa40771cc439
Xinyu Ma <maxin@flip3.engr.oregonstate.edu>
2020-02-19 15:15:20 -0800

    git commit client.py

--- Xinyu/client.py
+++ Xinyu/client.py
@@ -6,24 +6,24 @@ import threading
 #create a socket object
 def client(port,lamda):
 	num_socket = 4
-	for i in range (num_socket):
-		client = socket.socket(socket.AF_INET,socket.SOCK_STREAM)
-		hostname = socket.gethostname()
-		host =socket.gethostbyname(hostname)
+#	for i in range (num_socket):
+	client = socket.socket(socket.AF_INET,socket.SOCK_STREAM)
+	hostname = socket.gethostname()
+	host =socket.gethostbyname(hostname)
 
 		#define a port on which to connect
 
-		port = 8080+i
+#		port = 8080+i
 		#connect to server on local computer
-		client.connect((host,port))
+	client.connect((host,port))
 		#sample poisson random time
-		t = random.expovariate(2)
-		time.sleep(t)
+	t = random.expovariate(2)
+	time.sleep(t)
 		#receive data from the server
-		data = client.recv(1024)
-		s = data.decode()
-		client.sendall(s)
-		print(s)
+	data = client.recv(1024)
+	s = data.decode()
+	client.sendall(s)
+	print(s)
 #		check = (s=='Thank you for connecting')
 #		print(check)
 
