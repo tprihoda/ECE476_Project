8db25280c3e053afd5d5e72b05a961975d732145
tprihoda <prihodat@oregonstate.edu>
2020-02-19 20:59:34 -0800

    adjusting timing algorithm

--- Xinyu/client.py
+++ Xinyu/client.py
@@ -1,26 +1,43 @@
 import socket
+#import numpy as np
+import time
+import random
+import threading
 #create a socket object
-num_socket = 4
-for i in range (num_socket):
+def client(port,lamda):
+#	for i in range (num_socket):
     client = socket.socket(socket.AF_INET,socket.SOCK_STREAM)
-#	hostname = socket.gethostname()
-#	host =socket.gethostbyname(hostname)
-    host = '192.168.1.110'
+#hostname = socket.gethostname()
+#host =socket.gethostbyname(hostname)
+    host = "192.168.1.110"
 #define a port on which to connect
-#	port = 50000+i
-    port = 8080+i
+
+#		port = 8080+i
 #connect to server on local computer
     client.connect((host,port))
-    client.sendall(bytes("Hello", 'UTF-8'))
+    while(1):
+#sample poisson random time
+        t = random.expovariate(lamda)
+        time.sleep(t)
 #receive data from the server
-    data = client.recv(1024)
-    s = data.decode()
-    print(s)
-    check = (s=='Hello')
-    print(check)
-#	data = client.recv(1024)
-#	s = data.decode()
-#	print(s)
-	
+        data = "Hello"
+        tosend = data.encode()
+        client.sendall(tosend)
+        #data = client.recv(1024)
+        #s = data.decode()
+        #print(s)
+#		check = (s=='Thank you for connecting')
+#		print(check)
 
-	
+
+
+threading1 = threading.Thread(target = client,args= (8080,4))
+threading2 = threading.Thread(target = client,args= (8081,3))
+threading3 = threading.Thread(target = client,args= (8082,2))
+threading4 = threading.Thread(target = client,args= (8083,1))
+threading1.start()
+threading2.start()
+threading3.start()
+threading4.start()
+
+
